6988ba76e808a72dda3af8593c5032f980d8fdaf
M-Taqi796 <itstaqi2919@gmail.com>
2025-09-25 21:10:22 +0500

    MeetingCard Component Added

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -2,6 +2,7 @@ import Blog from "../components/Blog/blog.component"
 import Features from "../components/Features/features.component"
 import Founder from "../components/Founder/founder.component"
 import Header from "../components/Header/header.component"
+import MeetingCard from "../components/MeetingCard/meetingCard.component"
 import Services from "../components/Services/services.component"
 
 const Home =() => {
@@ -12,6 +13,7 @@ const Home =() => {
             <Founder />
             <Blog />
             <Services />
+            <MeetingCard />
         </section>
     )
 }
