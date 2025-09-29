e1c826baa12e076adc8ccdb61aa20cd2d51e43ac
M-Taqi796 <itstaqi2919@gmail.com>
2025-09-29 10:54:56 +0500

    NavBar: Active pages will show bold font

--- src/components/NavBar/navBar.component.jsx
+++ src/components/NavBar/navBar.component.jsx
@@ -1,15 +1,14 @@
-import { Link } from "react-router-dom";
-import SecondryBTN from "../Button/secondryButton.component";
+import { NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="flex justify-between items-center px-20 mt-6 mb-10">
       <img className="h-16" src="/Logo.avif" alt="" />
       <div className="flex gap-8">
-        <Link to="/">Home</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/case-study">Case Study</Link>
-        <Link to="/about">About us</Link>
-        <Link to="/contact">Contact us</Link>
+        <NavLink className={({isActive})=> isActive? "font-bold" : ""} to="/">Home</NavLink>
+        <NavLink className={({isActive})=> isActive? "font-bold" : ""} to="/services">Services</NavLink>
+        <NavLink className={({isActive})=> isActive? "font-bold" : ""} to="/case-study">Case Study</NavLink>
+        <NavLink className={({isActive})=> isActive? "font-bold" : ""} to="/about">About us</NavLink>
+        <NavLink className={({isActive})=> isActive? "font-bold" : ""} to="/contact">Contact us</NavLink>
       </div>
       <button className="text-fontWhite px-6 py-3 bg-secondary shadow-[4px_4px_0px_0px] shadow-backgroundSecondary hover:shadow-[0px_0px_0px_0px]">Schedule a meeting</button>
     </nav>
