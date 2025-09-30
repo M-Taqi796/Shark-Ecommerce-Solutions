c634a3897129d2131599e3f7ccb950229ac7c150
M-Taqi796 <itstaqi2919@gmail.com>
2025-09-30 19:35:57 +0500

    Added: Audit Page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import CaseStudy from "./routes/caseStudy"
 import NavBar from "./components/NavBar/navBar.component"
 import Footer from "./components/Footer/footer.component"
 import LoadingBar from "./components/LoadingBar/loadingBar.component"
+import Audit from "./routes/audit"
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/case-study" element={<CaseStudy />} />
+          <Route path="/audit" element={<Audit />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/routes/audit.jsx
@@ -0,0 +1,12 @@
+import AuditForm from "../components/AuditForm/auditForm.component"
+import MeetingCard from "../components/MeetingCard/meetingCard.component"
+
+const Audit = () => {
+    return (
+        <section>
+            <AuditForm/>
+            <MeetingCard/>
+        </section>
+    )
+}
+export default Audit
