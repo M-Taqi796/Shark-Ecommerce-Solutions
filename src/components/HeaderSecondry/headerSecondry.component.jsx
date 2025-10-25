1001ab995ef3d5ed70a53b27326c40886eb5056b
M-Taqi796 <itstaqi2919@gmail.com>
2025-10-25 19:14:01 +0500

    service page made responsive

--- src/components/HeaderSecondry/headerSecondry.component.jsx
+++ src/components/HeaderSecondry/headerSecondry.component.jsx
@@ -3,30 +3,30 @@ import { motion } from "framer-motion"
 
 const HeaderSecondry = () => {
   return (
-    <header className="mx-40 mb-20 flex justify-between items-center mt-20">
-      <article className="flex flex-col gap-4 w-[50%]">
+    <header className="mx-20 mb-20 flex gap-6 justify-center items-center mt-20 max-xl:flex-col max-md:mx-6">
+      <article className="flex flex-col gap-4 w-full items-center">
         <motion.div initial={{y:50, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration:0.5}}>
-        <h1 className="text-xl">
+        <h1 className="text-xl max-sm:text-sxl max-xl:text-center">
           Turning Amazon Clicks Into <br />
           <span className="text-secondary">Consistent</span>{" "}
           <span className="text-primary">Sales</span>
         </h1>
         </motion.div>
         <motion.div initial={{y:50, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration:0.5, delay:0.5}}>
-        <p className="text-md">Proven PPC & listing optimisation that actually delivers.</p>
+        <p className="text-md max-sm:text-sm max-xl:text-center">Proven PPC & listing optimisation that actually delivers.</p>
         </motion.div>
         <motion.div initial={{y:50, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration:0.5, delay:1}}>
-        <div className="flex gap-4">
-        <button className="text-fontWhite w-72 py-3 bg-secondary shadow-[4px_4px_0px_0px] shadow-backgroundSecondary hover:shadow-[0px_0px_0px_0px]">
+        <div className="flex gap-4 max-md:flex-col">
+        <button className="text-fontWhite px-2 w-72 py-3 bg-secondary shadow-[4px_4px_0px_0px] shadow-backgroundSecondary hover:shadow-[0px_0px_0px_0px] max-md:w-full">
           Schedule a meeting
         </button>
-        <button className="text-fontPrimary w-72 py-3 bg-primary shadow-[4px_4px_0px_0px] shadow-backgroundSecondary hover:shadow-[0px_0px_0px_0px]">
+        <button className="text-fontPrimary px-2 w-72 py-3 bg-primary shadow-[4px_4px_0px_0px] shadow-backgroundSecondary hover:shadow-[0px_0px_0px_0px] max-md:w-full">
           Get a <span className="font-bold">Free</span> Audit Report
         </button>
         </div>
         </motion.div>
       </article>
-      <img className="w-[50%] rounded-tr-[6.25rem] rounded-bl-[6.25rem] rounded-tl-3xl rounded-br-3xl" src="meeting.png" alt="" />
+      <img className="w-full rounded-tr-[6.25rem] rounded-bl-[6.25rem] rounded-tl-3xl rounded-br-3xl max-sm:rounded-tr-[4rem] max-sm:rounded-bl-[4rem]" src="meeting.png" alt="" />
     </header>
   );
 };
--- src/components/ServicesMain/servicesMain.component.jsx
+++ src/components/ServicesMain/servicesMain.component.jsx
@@ -2,16 +2,16 @@ import ServicesCard from "../Services/core/ServicesCard.component";
 
 const Servicesmain = () => {
   return (
-    <section className="mb-20 mx-40">
-      <h1 className="text-xl text-secondary text-center">We Help You Rise</h1>
-      <p className="text-center mx-60">
+    <section className="mb-20 mx-20 max-sm:mx-4">
+      <h1 className="text-xl text-secondary text-center max-sm:text-sxl">We Help You Rise</h1>
+      <p className="text-center mx-40 text-sm max-sm:text-ssm max-[75rem]:mx-8 max-[62rem]:mx-2">
         From PPC campaigns to SEO, design, and listing optimization — we provide
         end-to-end Amazon solutions that boost visibility, increase sales, and
         strengthen your brand. Our strategies are built to help you grow
         smarter, faster, and higher.
       </p>
-      <div className="flex gap-5 mt-10">
-        <div className="flex flex-col gap-5">
+      <div className="flex gap-5 mt-10 max-lg:flex-col">
+        <div className="flex flex-col gap-5 w-full max-lg:flex-row max-sm:flex-col">
           <ServicesCard
             image="seo.svg"
             title="SEO"
@@ -23,7 +23,7 @@ const Servicesmain = () => {
             description="Create eye-catching visuals, A+ content, and storefront designs that build trust and attract more buyers."
           />
         </div>
-        <div className="w-[50%] justify-center bg-background shadow text-fontWhite px-5 py-8 rounded-2xl flex flex-col items-center gap-4 hover:bg-primary hover:scale-105 transition-transform duration-300 group">
+        <div className="w-full justify-center bg-background shadow text-fontWhite px-5 py-8 rounded-2xl flex flex-col items-center gap-4 hover:bg-primary hover:scale-105 transition-transform duration-300 group">
           <img className="size-28" src="ppc.svg" alt="" />
           <h2 className="text-md font-bold text-secondary group-hover:text-fontWhite">
             "Amazon PPC"
