3199cdc546dd969be4c0efb11de9b3f70aefd357
M-Taqi796 <itstaqi2919@gmail.com>
2025-10-26 01:34:09 +0500

    added: New Logo and Icons

--- src/components/Footer/footer.component.jsx
+++ src/components/Footer/footer.component.jsx
@@ -1,15 +1,18 @@
 const Footer =() => {
     return (
         <section className="bg-backgroundSecondary px-20 pt-20 pb-10 text-fontSecondary max-[53.75rem]:py-6 max-sm:px-6">
+            <div className="flex justify-center mb-4">
+            <img className="w-20" src="/Icon_Shark.svg" alt="" />
+            </div>
             <div className="flex justify-between mb-20 max-[53.75rem]:flex-col">
                 <div className="flex flex-col max-[53.75rem]:justify-center max-[53.75rem]:items-center max-[53.75rem]:mb-6 gap-3">
                     <h2 className="text-lg text-secondary">Services</h2>
                     <p className="text-center">Amazon PPC | Listing Optimisation | SEO | Designing</p>
                     <div className="flex gap-3">
-                        <img className="size-8" src="fb.svg" alt="" />
-                        <img className="size-8" src="ig.svg" alt="" />
-                        <img className="size-8" src="tiktok.svg" alt="" />
-                        <img className="size-8" src="x.svg" alt="" />
+                        <img className="size-8" src="/fb.svg" alt="" />
+                        <img className="size-8" src="/ig.svg" alt="" />
+                        <img className="size-8" src="/tiktok.svg" alt="" />
+                        <img className="size-8" src="/x.svg" alt="" />
                     </div>
                 </div>
                 <div className="flex flex-col max-[53.75rem]:justify-center max-[53.75rem]:items-center gap-3 items-end">
