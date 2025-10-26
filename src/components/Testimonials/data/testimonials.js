ef74ccda7e84e27440980d1c2c18debc0e09d4c2
M-Taqi796 <itstaqi2919@gmail.com>
2025-10-26 18:52:03 +0500

    testimonial image import issue resolved

--- src/components/Testimonials/data/testimonials.js
+++ src/components/Testimonials/data/testimonials.js
@@ -1,30 +1,26 @@
-import Default from "../../../../public/default.jpg";
-import Rushton from "../../../../public/Rushton.png";
-import Jack from "../../../../public/Jack.png";
-import Sienna from "../../../../public/Sienna.png";
-import Alexandra from "../../../../public/Alexandra.png";
+// For default pic use: "/default.jpg""
 
 const testimonials = [
   {
-    pic: Sienna,
+    pic: "/Sienna.png",
     name: "Sienna",
     stars: 5,
     text: "Kamran has skyrocketed my Amazon business. He has been invaluable to increasing sales, running ads, and handling day-to-day issues that arise. Every request I’ve made of him, he has fulfilled quickly and efficiently. He’s on top of the business and makes sure to let me know what is needed on my end while doing everything he can on his own. Working with Naeem has been the best decision I’ve made for my business. I would do it 100x again. I highly recommend him!!",
   },
   {
-    pic: Alexandra,
+    pic: "/Alexandra.png",
     name: "Alexandra",
     stars: 5,
     text: "Kamran is, without a doubt, the best Amazon brand manager we've worked with. His deep understanding of Amazon's platform, attention to detail, and proactive approach have greatly benefited our brand. He consistently delivers top-tier results, from optimizing listings to managing PPC campaigns, and his communication skills make the entire process smooth and efficient. If you're looking to take your Amazon business to the next level, Kamran is the person you want on your team. Highly recommended!",
   },
   {
-    pic: Jack,
+    pic: "/Jack.png",
     name: "Jack",
     stars: 5,
     text: "Kamran has consistently delivered exceptional performance in managing our advertising campaigns. They expertly fine-tune bidding strategies, monitor keyword performance, and adjust ad placements in real time to maximize return on ad spend. Their data-driven approach has led to noticeable improvements in our click-through and conversion rates, while their proactive responsiveness to market trends ensures that our campaigns remain competitive and effective",
   },
   {
-    pic: Rushton,
+    pic: "/Rushton.png",
     name: "Rushton",
     stars: 5,
     text: "Kamran is the man! He treats my business as though it's his own. What more could you ever want in an employee? He's an expert in dealing with Amazon support and their relentless policy changes and gliches",
